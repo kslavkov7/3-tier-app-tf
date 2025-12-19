b778c36ea2a3239fe8a3f8bd8b1afe7a031a51f6
Kiril Slavkov <kiko19141914@gmail.com>
2025-12-19 17:25:35 +0200

    tried adding backend tier but needs configuring

--- cloud-init.sh
+++ cloud-init.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
 # 1. Update OS
-apt update -y
+apt update -y && apt upgrade -y
 
 # 2. Install Docker
-apt install docker.io -y
+apt install -y docker.io
 
-# 3. Enable Docker
-systemctl start docker
+# 3. Enable and start Docker service
 systemctl enable docker
+systemctl start docker
 
 # 4. Pull frontend image from Docker Hub
 docker pull kslavkov7/3tier-app-tf:frontend
 
-# 5. Run frontend container
-docker run -d --name frontend -p 80:80 kslavkov7/3tier-app-tf:frontend
+# 5. Create systemd service for frontend container
+cat <<EOF > /etc/systemd/system/frontend.service
+[Unit]
+Description=Frontend Docker Container
+After=docker.service
+Requires=docker.service
+
+[Service]
+Restart=always
+ExecStart=/usr/bin/docker run --name frontend -p 80:80 --rm kslavkov7/3tier-app-tf:frontend
+ExecStop=/usr/bin/docker stop frontend
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# 6. Enable and start the frontend service
+systemctl daemon-reload
+systemctl enable frontend.service
+systemctl start frontend.service
